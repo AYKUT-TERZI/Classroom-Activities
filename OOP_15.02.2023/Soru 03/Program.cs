9805b44bd08cfbdc5d17b2fa12fea0d7e053847e
aykutterzi01 <aykut_terzi@hotmail.com>
2023-02-15 17:53:23 +0300

    Updates

---
+++ OOP_15.02.2023/Soru 03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Toplama toplama = new Toplama();
+
+            Console.WriteLine("Lütfen 1. sayıyı giriniz.");
+            toplama.Sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. sayıyı giriniz.");
+            toplama.Sayi2 = int.Parse(Console.ReadLine());
+
+            toplama.Toplam();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_15.02.2023/Soru 03/Toplama.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru_03
+{
+    internal class Toplama
+    {
+        public int Sayi1 { get; set; }
+        public int Sayi2 { get; set; }
+        
+        public void Toplam()
+        {
+            Console.WriteLine("Bu iki sayının toplamı: " + (Sayi1 + Sayi2));
+        }
+    }
+}
